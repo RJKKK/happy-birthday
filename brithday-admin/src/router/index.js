bb2741c38de7d0f8c1e7db84c1e1d590720557ac
RJKKK <46342425+RJKKK@users.noreply.github.com>
2020-08-27 09:32:49 +0800

    Add files via upload

---
+++ brithday-admin/src/router/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {HashRouter, Route, Switch,Redirect} from 'react-router-dom';
+import App from "../App";
+import MainContent from "../views/main-content.jsx";
+import UsersAdmin from '../views/usersAdmin.jsx'
+const Routes = ()=>(
+        <HashRouter>
+            <Switch>
+                <Route path={'/'} component={()=>(
+                    <App>
+                        <Route exact path={'/'} component={()=>(<Redirect to={'/main'} />)} />
+                        <Route path={'/main'} component={()=>(
+                            <MainContent>
+                                <Route exact path={'/main/'} component={()=>(<Redirect to={'/main/usersAdmin'} />)} />
+                                <Route exact path={'/main/usersAdmin'} component={UsersAdmin} />
+                            </MainContent>
+                        )}/>
+                    </App>)
+                } />
+            </Switch>
+        </HashRouter>
+    )
+export default Routes
